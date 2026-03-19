8c12cbe6973d736dfb263db76bbdc2f62999ac26
Cristian97s <cristianaleman1997@gmail.com>
2026-03-18 20:14:15 -0600

    feat: usos de tags

--- landing/views.py
+++ landing/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-
+from datetime import date
 # Create your views here.
 
 def home(request):
+    today = date.today()
+    stock = ['Django', 'Frappe', 'FastApi', 'Dotnet', 'Vue', 'React', 'python', 'csharp', 'javascript', 'sql']
     return render(request, "landing/landing.html", {
-        "name": "Crist"
+        "name": "Crist",
+        "today": today,
+        "age": 28,
+        "stock": stock
     })
