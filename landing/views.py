2d95e3ab51cd2194a3b1a620192993b24ff59172
Cristian97s <cristianaleman1997@gmail.com>
2026-03-19 23:17:30 -0600

    feat: urls dinámicas y url tag

--- landing/views.py
+++ landing/views.py
@@ -1,13 +1,26 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from datetime import date
 # Create your views here.
 
 def home(request):
     today = date.today()
-    stock = ['Django', 'Frappe', 'FastApi', 'Dotnet', 'Vue', 'React', 'python', 'csharp', 'javascript', 'sql']
+    stock = [ {'id':'Django', 'name': 'Django'}, 
+             {'id':'Frappe', 'name': 'Frappe'}, 
+             {'id':'FastApi', 'name': 'FastApi'}, 
+             {'id':'Dotnet', 'name': 'Dotnet'}, 
+             {'id':'Vue', 'name': 'Vue'}, 
+             {'id':'React', 'name': 'React'}, 
+             {'id':'python', 'name': 'python'}, 
+             {'id':'csharp', 'name': 'csharp'}, 
+             {'id':'javascript', 'name': 'javascript'}, 
+             {'id':'sql', 'name': 'sql'},]
     return render(request, "landing/landing.html", {
         "name": "Crist",
         "today": today,
         "age": 28,
         "stock": stock
     })
+
+def stack_detail(request, tool):
+    return HttpResponse(f"Tecnologia: {tool}")
