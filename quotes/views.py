a83de01477cc17fe54acef6d40acdb37e45a64ed
Cristian97s <cristianaleman1997@gmail.com>
2026-03-22 19:01:05 -0600

    feat: template 404

--- quotes/views.py
+++ quotes/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, Http404
 from django.urls import reverse
 # Create your views here.
 
@@ -33,4 +33,5 @@ def days_week(request, day):
         quote_text = days_of_week[day]
         return HttpResponse(quote_text)
     except KeyError:
-        return HttpResponseNotFound("Este dia no existe.")
+        # return HttpResponseNotFound("Este dia no existe.")
+        raise Http404()
